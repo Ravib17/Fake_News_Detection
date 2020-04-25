379629d316d6a62fc32b3d2fb8d93eb063fcfce9
Ravi Bhatia <ravihbhatia@gmail.com>
2020-04-25 22:30:53 +0530

    code.r is created

---
+++ code.r
@@ -0,0 +1,29 @@
+library(rtweet)
+news_data <- read_twitter_csv("india_news.csv",unflatten = FALSE)
+
+username <- c()
+tweet    <-c()
+activity <-c()
+verified <-c()
+country  <-c()
+followers <-c()
+isfake   <-c()
+
+for (row in 1:nrow(news_data)){
+  
+  username[row]=unlist(news_data[row,"screen_name"])
+  tweet[row]=unlist(news_data[row,"text"])
+  if(nrow(subset(news_data,news_data$screen_name==username[row]) ) > 17){
+    activity[row]="Suspecious"
+  }
+  else{
+    activity[row]="Normal"
+  }
+  verified[row]=unlist(news_data[row,"verified"])
+  country[row]=unlist(news_data[row,"location"])
+  followers[row]=unlist(news_data[row,"followers_count"])
+  isfake[row]= "No"
+  training_data = data.frame(username,tweet,activity,verified,country,followers,isfake);
+  save_as_csv(training_data, "training_data.csv", prepend_ids = TRUE, na = "", fileEncoding = "UTF-8")
+}
+
