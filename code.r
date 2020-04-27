5bd24fa24930efce0b2cb7758b12d4c7996b393b
Ravi Bhatia <ravihbhatia@gmail.com>
2020-04-27 11:38:52 +0530

    changes

--- code.r
+++ code.r
@@ -12,7 +12,7 @@ country  <-c()
 followers <-c()
 isfake   <-c()
 
-for (row in 1:nrow(news_data)){
+for (row in 1:20){
   if(is.na(news_data[row,"location"]) ==  FALSE ) {
     
     user_loc = nominatim::osm_geocode(unlist(news_data[row,"location"]),key=getOption("OSM_API_KEY","QKD8MG1qHwLyPAgmtZ9MTFAdHaHt1C5a"))
@@ -70,5 +70,66 @@ testing_data <- tweets_data[-tweet_index,]
 #View(training_data)
 #View(testing_data)
 
-save_as_csv(training_data, "training_data.csv", prepend_ids = TRUE, na = "", fileEncoding = "UTF-8")
-save_as_csv(testing_data, "testing_data.csv", prepend_ids = TRUE, na = "", fileEncoding = "UTF-8")
+save_as_csv(training_data, "training_data1.csv", prepend_ids = TRUE, na = "", fileEncoding = "UTF-8")
+save_as_csv(testing_data, "testing_data1.csv", prepend_ids = TRUE, na = "", fileEncoding = "UTF-8")
+
+chunks = extractChunks("r u able to understand google how to use it")
+
+library("NLP")
+library("openNLP")
+extractChunks <- function(x) {
+  
+  x <- as.String(x)
+  wordAnnotation <- annotate(x, list(Maxent_Sent_Token_Annotator(), Maxent_Word_Token_Annotator()))
+  POSAnnotation <- annotate(x, Maxent_POS_Tag_Annotator(), wordAnnotation)
+  POSwords <- subset(POSAnnotation, type == "word")
+  tags <- sapply(POSwords$features, '[[', "POS")
+  tokenizedAndTagged <- data.frame(Tokens = x[POSwords], Tags = tags)
+  
+  tokenizedAndTagged$Tags_mod = grepl("NN|JJ", tokenizedAndTagged$Tags)
+  chunk = vector()
+  
+  chunk[1] = as.numeric(tokenizedAndTagged$Tags_mod[1])
+  
+  for (i in 2:nrow(tokenizedAndTagged)) {
+    
+    if(!tokenizedAndTagged$Tags_mod[i]) {
+      chunk[i] = 0
+    } else if (tokenizedAndTagged$Tags_mod[i] == tokenizedAndTagged$Tags_mod[i-1]) {
+      chunk[i] = chunk[i-1]
+    } else {
+      chunk[i] = max(chunk) + 1
+    }
+    
+  }
+  
+  text_chunk <- split(as.character(tokenizedAndTagged$Tokens), chunk)
+  tag_pattern <- split(as.character(tokenizedAndTagged$Tags), chunk)
+  names(text_chunk) <- sapply(tag_pattern, function(x) paste(x, collapse = "-"))
+  
+  # Extract chunks matching pattern
+  res = text_chunk[grepl("JJ-NN|NN.-NN", names(text_chunk))]
+  res = sapply(res, function(x) paste(x, collapse =  " "))
+  print(res)
+  return(res)
+  
+  gc()
+  
+}
+
+GoogleHits <- function(input)
+{
+  require(XML)
+  require(RCurl)
+  url <- paste("https://www.google.com/search?q=",
+               input, sep = "") # modified line      
+  CAINFO = paste(system.file(package="RCurl"), "/CurlSSL/ca-bundle.crt", sep = "")
+  script <- getURL(url, followlocation = TRUE, cainfo = CAINFO)
+  doc <- htmlParse(script)
+  res <- xpathSApply(doc, '//*/div[@id="resultStats"]', xmlValue)
+  cat(paste("\nYour Search URL:\n", url, "\n", sep = ""))
+  cat("\nNo. of Hits:\n") # get rid of cat text if not wanted
+  return(as.integer(gsub("[^0-9]", "", res)))
+}
+library(httr)
+dat<-GET("https://www.googleapis.com/customsearch/v1?key=AIzaSyAUKRDBSuaXvzTEpn13OLkio5NVwTYZhis&cx=017137198569103930420:hpuryupuqkn&q=covid")
